df13d2e7458503d3622d03591f3fa0f18542af00
ThangSuperMan <thangjenny2002@gmail.com>
2021-06-03 18:30:39 +0700

    add jascript make a toggle menu and toggle icon menu and have some events when the window scroll the screen the menu bar will turn off

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
-let menu = document.querySelector("#menu-bar");
-let navbar = document.querySelector(".navbar")
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector(".navbar");
+
+menu.addEventListener('click', ()=> {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+});
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
